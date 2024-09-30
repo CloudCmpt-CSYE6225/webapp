dc41bc5675d2c416b9c11ba3aee4df408e14a3a9
Srijith Makam <makam.s@northeastern.edu>
2024-09-30 18:01:58 -0400

    user model added

---
+++ models/user.js
@@ -0,0 +1,48 @@
+import sequelize from "../config/database.js";
+import { DataTypes, Sequelize } from "sequelize";
+
+const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: Sequelize.UUIDV4,
+      primaryKey: true
+    },
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
+    account_created: {
+      type: DataTypes.DATE,
+      defaultValue: Sequelize.NOW
+    },
+    account_updated: {
+      type: DataTypes.DATE,
+      defaultValue: Sequelize.NOW
+    }
+  }, {
+    timestamps: false, 
+    // Hook to update account_updated
+    hooks: {
+      beforeUpdate: async (user) => {
+        user.account_updated = new Date();
+      }
+    }
+  });
+
+export default User;
