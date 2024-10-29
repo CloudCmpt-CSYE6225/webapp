151ba52c506d98d429c05abb36cd8d32837689fd
Srijith Makam <makam.s@northeastern.edu>
2024-10-29 17:08:51 -0400

    updated packer for cloudwatch

--- packer/scripts/install_dependencies.sh
+++ packer/scripts/install_dependencies.sh
@@ -6,3 +6,8 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get install -y nodejs npm unzip
 
 sudo apt-get clean
 sudo rm -rf /var/lib/apt/lists/*
+
+# Install and configure CloudWatch agent
+log "Installing CloudWatch agent"
+sudo apt-get install -y amazon-cloudwatch-agent
+sudo systemctl enable amazon-cloudwatch-agent
