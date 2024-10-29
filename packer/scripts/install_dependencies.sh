c948ac775c8ec77823fd83a80b5fc0d859375423
Srijith Makam <makam.s@northeastern.edu>
2024-10-29 18:14:54 -0400

    cloudwatch install error

--- packer/scripts/install_dependencies.sh
+++ packer/scripts/install_dependencies.sh
@@ -2,17 +2,17 @@
 set -e
 
 sudo apt-get update
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y nodejs npm unzip
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y nodejs npm unzip wget
 
 #adding cloudwatch configuration
 echo "CloudWatch Agent downloading..."
-cd
-ls 
-wget https://s3.amazonaws.com/amazoncloudwatch-agent/amazon_linux/amd64/latest/amazon-cloudwatch-agent.rpm
-sudo rpm -U ./amazon-cloudwatch-agent.rpm
+cd /tmp
+wget https://s3.amazonaws.com/amazoncloudwatch-agent/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
+sudo dpkg -i -E ./amazon-cloudwatch-agent.deb
+
 echo "Cloudwatch agent downloaded successfully!!"
 
-
+rm -f amazon-cloudwatch-agent.deb
 sudo apt-get clean
 sudo rm -rf /var/lib/apt/lists/*
 
